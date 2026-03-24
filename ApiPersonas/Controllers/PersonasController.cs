a213639f8e09ad3809957109252af0e5918227de
raulgar19 <raulgamu2003@gmail.com>
2026-03-24 11:44:13 +0100

    Add project files.

---
+++ ApiPersonas/Controllers/PersonasController.cs
@@ -0,0 +1,18 @@
+﻿using ApiPersonas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPersonas()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            return Ok(personas);
+        }
+    }
+}
